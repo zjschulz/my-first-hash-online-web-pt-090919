9cd9a3f646102fba5bb25fa7e32e80b8219d8307
Zachary Schulz <zjschulz@gmail.com>
2019-09-22 16:19:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,15 +25,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets"] = 2 
   shipping_manifest["gun powder"] = 4
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+  shipping_manifest
 end
