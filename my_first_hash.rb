699fb6e17b9c05ed3fb57046a5276b6187ed78e9
Zachary Schulz <zjschulz@gmail.com>
2019-09-22 16:15:11 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,7 +7,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+  your_hash = {"whale" => "value", "another_key" => "another value"}
 
 end
 
